17ab61ee29f33a0a1238241764b1929383b5cc22
Pavel <Qw3st@mail.ru>
2022-12-18 11:18:07 +0300

    ex_011

---
+++ Seminar/Example_011/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Система сгенерировала случайное число {number}");
+
+int Firstnum = number / 100;
+Console.WriteLine($"Первое число равно: {Firstnum}");
+int Varnum = (number/10);
+int Secondnum = number - Varnum*10;
+Console.WriteLine($"Третье число равно: {Secondnum}");
+int FinalNum = Firstnum*10 + Secondnum;
+
+Console.WriteLine(FinalNum);
