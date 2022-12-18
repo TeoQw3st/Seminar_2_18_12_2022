094f06ffb38f2d771b2b1def5f7e8fdcf8f6e290
Pavel <Qw3st@mail.ru>
2022-12-18 11:01:11 +0300

    ex_012

---
+++ Seminar/Example_012/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 % number2 == 0)
+{
+    Console.Write("Второе число кратно первому");
+}
+else
+{
+    Console.Write($"Второе число не кратно первому, остаток равен {number1 % number2}");
+
+}
