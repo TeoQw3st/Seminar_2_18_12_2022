4b2e74417485ff13931efa66c7adf41aa6194847
Pavel <Qw3st@mail.ru>
2022-12-23 15:16:37 +0300

    ex_013

---
+++ DZ/.gitignore/Example_013/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Для решения задачи №13 введите любое число ");
+    Int64 number = Convert.ToInt64(Console.ReadLine());
+    Console.WriteLine ($"Введеное число: {number}");
+    string massive = number.ToString();
+    //Console.WriteLine ($"Массив: {massive}");
+    int size = massive.Length;
+    //Console.WriteLine ($"Размер массива: {size}");
+    if (size < 3)
+    {
+    Console.WriteLine("Введено не трехзначное число ");
+    }
+    else
+    {
+      Console.WriteLine($"Третья цифра в введенном числе равна: {massive[2]}");
+    }
